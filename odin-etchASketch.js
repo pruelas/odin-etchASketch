88b4c7bf53942577b33883aaae6b4c823f9f42f2
Pedro Ruelas <pruelas5238@yahoo.com>
2022-02-09 22:19:05 -0800

    Added clear grid functionality.

--- odin-etchASketch.js
+++ odin-etchASketch.js
@@ -1,8 +1,38 @@
 //create 16x16 etchASketch grid by appending child nodes to parent div
 
-btn.addEventListener('click', function (e) {
+function generateGrid(x, parentDiv){
+    for(var i = 0; i < x; i++){
+        const childRow = document.createElement("div");
+        childRow.id = "row";
+        parentDiv.appendChild(childRow);
+        for(var j = 0; j < x; j++){
+            const childDiv = document.createElement("div");
+            childDiv.classList = "childDiv";
+            childDiv.id = "child" + i;
+            childRow.appendChild(childDiv);
+        }
+    }
+
+}
+
+
+var btns = document.querySelectorAll(".btn");
+console.log(btns);
+btns.forEach(btn => btn.addEventListener('click', function (e) {
 
     const parentDiv = document.getElementById("grid");
+    const clearBtn = document.getElementById("clearBtn");
+    if(e.target.textContent == "Start Grid!"){
+        generateGrid(16, parentDiv);
+        clearBtn.style.display = "initial";
+    }else{
+        console.log(e);
+        while(parentDiv.hasChildNodes()){
+            parentDiv.removeChild(parentDiv.lastChild);
+        }
+        generateGrid(64, parentDiv);
+    }
+    /*
     for(var i = 0; i < 32; i++){
         const childRow = document.createElement("div");
         childRow.id = "row";
@@ -13,9 +43,11 @@ btn.addEventListener('click', function (e) {
             childDiv.id = "child" + i;
             childRow.appendChild(childDiv);
         }
-    }
+    }*/
 
-});
+
+
+}));
 
 var etchASketchTiles = document.getElementById("grid");
 etchASketchTiles.addEventListener('mouseover', function (e) {
@@ -24,3 +56,12 @@ etchASketchTiles.addEventListener('mouseover', function (e) {
         e.target.style.backgroundColor = 'black';
     }
 });
+
+/*
+btns.forEach(btn => {
+
+    btn..addEventListener('click', (event)=> {
+     // ... event handled
+    });
+ 
+ });*/
